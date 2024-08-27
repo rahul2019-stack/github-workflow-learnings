15aed32564ec5e2dadb8cec7c0bf96e21a27bcf7
Rahul Yadav <G698903@genmills.com>
2024-08-27 21:30:04 +0530

    Changing name of file

--- tests/test_src_code.py
+++ tests/test_src_code.py
@@ -1,6 +1,6 @@
 import pytest
-from src.sample import add_numbers
+from src.main import add_numbers
 
 def test_add_numbers():
     # Test cases
-    assert add_numbers(1, 2) == 3 
+    assert add_numbers(1, 2) == 3
